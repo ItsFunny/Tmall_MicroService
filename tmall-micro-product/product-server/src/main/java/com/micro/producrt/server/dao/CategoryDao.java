68c726b825b485fb5a244a137f549fb36da9ff0b
joker <18757883747@163.com>
2018-05-25 13:21:25 +0800

    add product

--- tmall-micro-product/product-server/src/main/java/com/micro/producrt/server/dao/CategoryDao.java
+++ tmall-micro-product/product-server/src/main/java/com/micro/producrt/server/dao/CategoryDao.java
@@ -6,18 +6,24 @@
 */
 package com.micro.producrt.server.dao;
 
+import java.util.Collection;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import com.micro.product.common.dto.CategoryDTO;
+
 /**
-* 
-* @author joker 
-* @date 创建时间：2018年5月25日 上午10:41:15
-*/
+ * 
+ * @author joker
+ * @date 创建时间：2018年5月25日 上午10:41:15
+ */
 @Mapper
 public interface CategoryDao
 {
 	@Select("select count(1) form tmall_category")
 	Integer countCategory();
-	
+
+	@Select("select category_id,parent_category_id,category_name,create_date,update_date from tamll_category")
+	Collection<CategoryDTO> findAll();
 }
---
+++ tmall-micro-product/product-server/src/main/java/com/micro/producrt/server/service/ICategoryService.java
@@ -0,0 +1,23 @@
+/**
+*
+* @author joker 
+* @date 创建时间：2018年5月25日 下午1:17:16
+* 
+*/
+package com.micro.producrt.server.service;
+
+import java.util.Collection;
+
+import com.micro.product.common.dto.CategoryDTO;
+
+/**
+* 
+* @author joker 
+* @date 创建时间：2018年5月25日 下午1:17:16
+*/
+public interface ICategoryService
+{
+	Integer countCateogry();
+	
+	Collection<CategoryDTO>findAll();
+}
